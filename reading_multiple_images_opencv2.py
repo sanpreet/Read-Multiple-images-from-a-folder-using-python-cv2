5b3ff262584fc4a619bdf17ad22f65a77d1e904a
Sanpreet Singh <sany26.com@gmail.com>
2018-08-16 01:31:55 +0530

    Add files via upload

---
+++ reading_multiple_images_opencv2.py
@@ -0,0 +1,42 @@
+#import the library opencv
+import cv2
+#globbing utility.
+import glob
+#select the path
+path = "A:\MY_company\Sanpreet_Singh\Client Work\OpenCV-Sanjeev\images\*.*"
+for file in glob.glob(path):
+    print(file)
+    a= cv2.imread(file)
+    print(a)
+    # %%%%%%%%%%%%%%%%%%%%%
+    #conversion numpy array into rgb image to show
+    c = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+    cv2.imshow('Color image', c)
+    #wait for 1 second
+    k = cv2.waitKey(1000)
+    #destroy the window
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
