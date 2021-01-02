048f727040c0ade93ddfafecfbba46c0cab54e7d
Sanpreet Singh <sany26.com@gmail.com>
2021-01-02 22:15:06 +0530

    Update reading_multiple_images_opencv2.py

--- reading_multiple_images_opencv2.py
+++ reading_multiple_images_opencv2.py
@@ -4,16 +4,13 @@ import cv2
 import glob
 #select the path
 #I have provided my path from my local computer, please change it accordingly
-path = "A:\MY_company\Sanpreet_Singh\Client Work\OpenCV-Sanjeev\images\*.*"
+path = "A:\images\\input_images\*.*"
 for file in glob.glob(path):
-    print(file)
-    a= cv2.imread(file)
-    print(a)
-    # %%%%%%%%%%%%%%%%%%%%%
-    #conversion numpy array into rgb image to show
-    c = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+    image_read = cv2.imread(file)
+    # conversion numpy array into rgb image to show
+    c = cv2.cvtColor(image_read, cv2.COLOR_BGR2RGB)
     cv2.imshow('Color image', c)
-    #wait for 1 second
+    # wait for 1 second
     k = cv2.waitKey(1000)
-    #destroy the window
+    # destroy the window
     cv2.destroyAllWindows()
